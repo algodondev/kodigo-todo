9612968fb1df6bc788d71d84cbde57a4ceef4b23
algodondev <179851883+algodondev@users.noreply.github.com>
2025-09-05 21:03:07 -0600

    feat: created container for to-dos

---
+++ src/components/TodoContainer.jsx
@@ -0,0 +1,13 @@
+import './TodoContainer.css';
+
+function TodoContainer() {
+  return (
+    <div className="app-container">
+      <div className="app-container todo-main">
+        <h1 className="app-container todo-main title">Kodigo's To-Do</h1>
+      </div>
+    </div>
+  );
+}
+
+export default TodoContainer;
